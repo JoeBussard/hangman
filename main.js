e98207efd778946df9561a9450e5c605d2d92b5b
Joe Bussard <joeb360@gmail.com>
2019-01-25 20:33:11 -0800

    Debugging mostly

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ function secret_word(){
 /************** function play_game()
 This is the workhorse function of the entire program.  This is what is called when the start button is clicked.  This function runs the entire game from start to finish.  It calls multiple other functions that are also at the global scope level to this file.  It starts with declaring all the variables: The secret word and its array, the current guess, the progress/current word and its array, the array of incorrect letters, and the number of lives.
 It selects a random word from the dictionary using get_word_from_dictionary(rand);  It also has a piece of jquery animation for fun.**/
+
 function play_game() {
   console.log("anything???");
   //get_word_from_dictionary(123);
@@ -33,18 +34,22 @@ function play_game() {
   var lives = 6; //edit this to make it more difficult
                  // arm + arm + leg + leg + body + head = 6
   //set up our word
-  var word_from_dictionary = get_word_from_dictionary("foo");
+  var word_from_dictionary = get_word_from_dictionary_rando("foo");
   $('.index-header-class').delay(1).hide(500).delay(1000).show(1000);
   var wrong_array = [];
+  console.log("Escaped word from dictionary");
   do {
     secret = secret_word();
   } while (!secret);
   console.log(secret);
-//  secret_array = word_from_dictionary.split("");  
+  secret_array = word_from_dictionary.split("");  
+
   current_array = fill_current_with_blanks(secret_array);
   console.log(secret_array);
   
 //replace the contents of this loop with a function
+
+// This loop is the game function after the secret word has been set up.
 while ((lives > 0) && (!is_equal(secret_array, current_array))) {
   update_lives(lives);
   update_user(current_array);
@@ -77,8 +82,11 @@ while ((lives > 0) && (!is_equal(secret_array, current_array))) {
   //alert("He dies of dysentery fording a river 2 months later.");
   update_current(current_array);
   }
+
+// This is the end game.  If you get to this point, then the game is over.
   if (lives == 0) alert("You've lost the game.  Your hangman is hanged.");
-  else {
+// TODO need to change alert(".....") to innerHTML;
+else {
     alert("You've won the game! Your hangman is cut free.");
     alert("He dies of dysentery fording a river 2 months later.");
   }
@@ -86,6 +94,8 @@ while ((lives > 0) && (!is_equal(secret_array, current_array))) {
 }
 
 //alert() the user the wrong guesses.
+//the function will run an alert that tells the user which letters they
+//have guessed, but weren't in the word.
 function update_wrong(wrong) {
   if (!wrong.length) return;
   else {
@@ -95,6 +105,11 @@ function update_wrong(wrong) {
     }
     value += wrong[wrong.length - 1] + "."
     alert(value);
+
+// TODO change alert(value) to innerHTML;
+
+
+
   }
 }
 	
@@ -173,6 +188,36 @@ function check_guess(guess, secret_array, curr) {
 
 
 
+function get_word_from_dictionary_main(random) {
+  console.log("entered rando.js");
+  console.log("entered get word from dictionary");
+  var rand = random;
+  var dictionary;
+  var xhttp = new XMLHttpRequest();
+  console.log('a');
+  xhttp.onreadystatechange = function() {
+  console.log('d');
+    if (this.readyState == 4 && this.status == 200) {
+      console.log('b');
+     //console.log(this.responseText);//[Math.floor(Math.random()*this.responseTe    xt.length)]);
+      dictionary = JSON.parse(this.responseText);
+      var random_word = Object.keys(dictionary);
+      for (x in dictionary){
+    
+      }   
+      var index = Math.floor(Math.random()*random_word.length);
+      console.log("random word is " + random_word[index])
+      console.log("definition is " + Object.values(dictionary)[index]);
+    }   
+  };  
+  console.log('c');
+  xhttp.open("GET", "dictionary.json", true);
+  xhttp.send();
+  console.log(Math.random());
+
+}
+
+
 
 
 
@@ -180,15 +225,15 @@ function check_guess(guess, secret_array, curr) {
 function get_word_from_dictionary(random) {
   console.log("entered get word from dictionary");
   var rand = random;
-  word_to_return = "";
+  var word_to_return = "";
   var xhttp = new XMLHttpRequest();
   console.log('a');
   xhttp.onreadystatechange = function() {
   console.log('d');
-    if (this.readyState == 4 && this.status == 200) {
+    if ( (this.readyState == 4 && this.status == 200) || true) {
       console.log('b');
-      word_to_return = this.responseText[Math.floor(Math.random()*this.responseText.length)]l
-      console.log(this.responseText[Math.floor(Math.random()*this.responseText.length)]);
+      word_to_return = this.responseText[Math.floor(Math.random()*this.responseText.length)];
+      console.log(     this.responseText[Math.floor(Math.random()*this.responseText.length)]);
       return word_to_return;
     }
   }//;
--- rando.js
+++ rando.js
@@ -4,6 +4,7 @@ function get_word_from_dictionary_rando(random) {
   var rand = random;
   var dictionary;
   var xhttp = new XMLHttpRequest();
+  var current = "Debug";
   console.log('a');
   xhttp.onreadystatechange = function() {
   console.log('d');
@@ -18,14 +19,21 @@ function get_word_from_dictionary_rando(random) {
       var index = Math.floor(Math.random()*random_word.length);
       console.log("random word is " + random_word[index])
       console.log("definition is " + Object.values(dictionary)[index]);
+      update_curr_paragraph(random_word[index]);
+      update_def_paragraph(Object.values(dictionary)[index]);
     }
   };
   console.log('c');
   xhttp.open("GET", "dictionary.json", true);
   xhttp.send();
   console.log(Math.random());
-
 }
  
- 
 
+function update_curr_paragraph(current) {
+  document.getElementById('current_word').innerHTML = current;
+}
+
+
+function update_def_paragraph(def) {
+  document.getElementById('definition').innerHTML = def}
